292095a863d092e56db532064622f3aeb15c99be
daeekim <asdxl1030@naver.com>
2024-10-20 20:42:27 +0900

    minitalk

---
+++ 42cursus_05_minitalk/src/minitalk_utils.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: daeekim <daeekim@student.42gyeongsan.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 13:26:22 by daeekim           #+#    #+#             */
+/*   Updated: 2024/08/31 19:14:36 by daeekim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minitalk.h"
+
+int	ft_atoi(const char *str)
+{
+	int				sign;
+	long long		result;
+
+	sign = 1;
+	result = 0;
+	if (*str == '-')
+		sign *= -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	result = result * sign;
+	return ((int)result);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	long	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		ft_putchar('-');
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + '0');
+}
+
+void	ft_putstr(char *s)
+{
+	int	i;
+
+	if (!s)
+		s = "(null)";
+	i = -1;
+	while (s[++i])
+		ft_putchar(s[i]);
+}
+
+int	ft_isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
